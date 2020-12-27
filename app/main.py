7a16f2953e7c0e52280a5fd90caa28c756f1888e
Min Shin Khant <minshinkhant7@gmail.com>
2020-12-27 04:18:55 -0500

    adding cookies

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 
 app = FastAPI()
 
@@ -14,3 +14,7 @@ def read_root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
+@app.get("/cookies/")
+def create_cookie(response: Response):
+    response.set_cookie(key="fakesession", value="fake-cookie-session-value")
+    return {"message": "got the cookies"}
